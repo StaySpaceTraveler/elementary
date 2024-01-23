e53f15d1ff8c1453188f2ae328469a6b1d015f8e
StaySpaceTraveler <pencipenny@yandex.ru>
2024-01-23 14:24:55 +0300

    3.11 Логическое отрицание

--- src/main/java/ru/condition/LogicNot.java
+++ src/main/java/ru/condition/LogicNot.java
@@ -15,15 +15,16 @@ public class LogicNot {
     }
 
     public static boolean notPositive(int num) {
-        return !isPositive(num); /* или лучше num < 0 ?
-        */
+        return !isPositive(num);
     }
 
     public static boolean notEvenAndPositive(int num) {
+
         return notEven(num) && isPositive(num);
     }
 
     public static boolean evenOrNotPositive(int num) {
-        return isEven(num) || !notPositive(num);
+
+        return isEven(num) || notPositive(num);
     }
 }
